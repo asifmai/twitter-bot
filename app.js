f2abeacedef35af6c744f1f53770195a5c01a027
iqbal <asifmai@hotmail.com>
2020-02-19 14:10:12 +0500

    'update'

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 const pupHelper = require('./puppeteerhelper');
-const fs = require('fs');
-const urls = require('./urls');
 
-(async () => {
-  const results = [];
-  const browser = await pupHelper.launchBrowser();
-
-  for (let i = 0; i < urls.length; i++) {
-    console.log(`${i+1}/${urls.length} - Crawling...`);
+const fetchDetails = async (url) => {
+  try {
+    console.log(`Crawling from ${url}...`);
     const result = {};
+    const browser = await pupHelper.launchBrowser();
     const page = await pupHelper.launchPage(browser);
-    await page.goto(urls[i], {timeout: 0, waitUntil: 'load'});
+    await page.goto(url, {timeout: 0, waitUntil: 'load'});
     await page.waitForSelector('div[aria-label][role="group"]');
     const tagText = await pupHelper.getAttr('div[aria-label][role="group"]', 'aria-label', page);
-
+  
     const repliesRegEx = /\d+(?=\s*replies)/gi;
     const retweetsRegEx = /\d+(?=\s*retweets)/gi;
     const likesRegEx = /\d+(?=\s*likes)/gi;
     
-    result.url = urls[i];
+    result.url = url;
     if (repliesRegEx.test(tagText)) result.replies = tagText.match(repliesRegEx)[0].trim();
     if (retweetsRegEx.test(tagText)) result.retweets = tagText.match(retweetsRegEx)[0].trim();
     if (likesRegEx.test(tagText)) result.likes = tagText.match(likesRegEx)[0].trim();
-
-    if (result.replies && result.retweets && result.likes) results.push(result);
-
+  
     await page.close();
+    await browser.close();
+    return result;
+  } catch (error) {
+    console.log(`fetchDetails [${url}] Error: ${error.message}`);
+    return error;
   }
+}
 
-  fs.writeFileSync('results.json', JSON.stringify(results));
-  await browser.close();
-})()
+fetchDetails('https://twitter.com/coliegestudent/status/1218017231344472064').then(res => console.log(res));
--- urls.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  'https://twitter.com/coliegestudent/status/1218017231344472064',
-  'https://twitter.com/LexiNajera/status/1220508392529309699',
-  'https://twitter.com/sylviapowelll/status/1223459897733500928',
-  'https://twitter.com/angelicaschul/status/1223464295599628289',
-  'https://twitter.com/itsthetaylorjoy/status/1225966442249674753'
-]
